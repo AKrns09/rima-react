21723bf519e4de327e939473c1a401263511ee4d
Aaron Kearns <x20513033@student.ncirl.ie>
2024-07-31 22:11:45 +0100

    updated deployed backend link

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -8,7 +8,7 @@ const options = {
 const client = applyCaseMiddleware(
   axios.create({
     //baseURL: "http://127.0.0.1:3000",
-    baseURL: "http://ec2-52-210-36-47.eu-west-1.compute.amazonaws.com",
+    baseURL: "http://ec2-63-34-162-120.eu-west-1.compute.amazonaws.com",
   }),
   options
 );
